2df38bba7ddb5051d56dce81f79d504df57c7e3d
monpeco <monpeco@gmail.com>
2018-08-28 11:13:23 +0000

    Add asynchronous example

---
+++ 01-overview-javascript/examples/5-async.js
@@ -0,0 +1,13 @@
+function printOne(){
+  console.log("one")
+}
+function printTwo(){
+  console.log("two")
+}
+function printThree(){
+  console.log("three")
+}
+
+setTimeout(printOne, 1000)
+setTimeout(printTwo, 0)
+printThree()
