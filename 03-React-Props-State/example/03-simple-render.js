28db0875b42dd02ac7fcaa747b6b016667306d8a
monpeco <monpeco@gmail.com>
2018-11-17 19:19:39 +0000

    Add a simple render - codesandbox

---
+++ 03-React-Props-State/example/03-simple-render.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { render } from "react-dom";
+
+const App = () => {
+  return (
+    <div>
+      <h2>Hello</h2>
+      <p>Start editing ...</p>
+    </div>
+  );
+};
+
+render(<App />, document.getElementById("root"));
